bf33bea5764a44ca2f5b4e80b44f7a3bd813e1fb
Vladislav T <vt118@mail.ru>
2020-04-17 15:06:32 +0300

    feat: add cardList topic render on navMenuItem click

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,35 @@
 import cards from './cards';
-// import CardList from './js/cardList';
+import CardList from './cardList';
 import { MODES } from './constants';
 
 export default class App {
   constructor() {
     this.cards = cards;
+    this.cardContainer = document.querySelector('.cards');
     this.state = MODES.train;
     this.nav = document.querySelector('.nav');
+    this.serializedCards = this.getCardsSerialize();
   }
 
   init() {
     this.nav.addEventListener('click', (e) => {
-      if (e.target.classList.contains('nav__item')) {
-          const setListTitle = e.target.innerText;
-          console.log(setListTitle);
-      }
+      this.cardListRenderOnMenuItemClick(e);
     });
+  }
+
+  cardListRenderOnMenuItemClick(e) {
+    if (e.target.classList.contains('nav__item')) {
+      const setListTitle = e.target.getAttribute('data');
+      console.log(setListTitle);
+      this.clearCardContainer();
+      new CardList(this.serializedCards[setListTitle]).cardListRender();
+    }
+  }
+
+  clearCardContainer() {
+    while (this.cardContainer.firstChild) {
+      this.cardContainer.removeChild(this.cardContainer.lastChild);
+    }
   }
 
   getCardsSerialize() {
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,20 +1,20 @@
 import { wordSets } from './cards';
 
 export default class Nav {
-    constructor() {
-        this.nav = document.querySelector('.nav');
-    }
+  constructor() {
+    this.nav = document.querySelector('.nav');
+  }
 
-    renderNavigation() {
-        console.log(this.nav);
-        let fragment = this.renderNavItem('Main Page');
-        wordSets.forEach(item => {
-            fragment += this.renderNavItem(item);
-        });
-        this.nav.insertAdjacentHTML('afterbegin', fragment);
-    }
+  renderNavigation() {
+    console.log(this.nav);
+    let fragment = this.renderNavItem('Main Page');
+    wordSets.forEach(item => {
+      fragment += this.renderNavItem(item);
+    });
+    this.nav.insertAdjacentHTML('afterbegin', fragment);
+  }
 
-    renderNavItem(item) {
-        return `<li><a class="nav__item">${item}</a></li>`;
-    }
+  renderNavItem(item) {
+    return `<li><a class="nav__item" data="${item}">${item}</a></li>`;
+  }
 }
